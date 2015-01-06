ded1f8bae867fd29f30cc8323641979edeb68c2d
srb_nikola <milja1337@gmail.com>
2015-01-06 22:26:00 +0100

    no message

--- app.py
+++ app.py
@@ -65,6 +65,7 @@ hashDict = {
     'http://www.b92.net/info/rss/tehnopolis.xml': 1,
     'http://blog.b92.net/rss/feed/index.xml': 1,
     'http://www.b92.net/info/rss/automobili.xml': 1,
+    'http://www.b92.net/info/rss/biz.xml': 1,
     'http://www.b92.net/info/rss/news.xml': 1,
     'http://www.b92.net/info/rss/kultura.xml': 1,
     'http://www.b92.net/info/rss/putovanja.xml': 1,
